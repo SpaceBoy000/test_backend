715f6eef1493cbeac7b7ed3b8b778f82d18bb9b7
spaceboy000 <cryptospaceboy00@gmail.com>
2024-05-09 06:11:35 +0000

    update mongodb url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const DATABASE_URL = 'mongodb://127.0.0.1:27017/unitywallet';
+const DATABASE_URL = 'mongodb+srv://blockdev:osyU77rxNXIS3XWF@cluster0.zfnr6he.mongodb.net/unitywallet';
 const LP_COUNT = 10;
 
 module.exports = {
