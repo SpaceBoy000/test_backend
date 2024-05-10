a6fb1f49540dae15b0f9ccef29fb2e1b00720a61
spaceboy000 <cryptospaceboy00@gmail.com>
2024-05-10 06:11:49 +0000

    disable service

--- src/index.js
+++ src/index.js
@@ -19,18 +19,6 @@ app.get('/hello', (req, res) => {
     return res.status(200).send("How are you?");
 })
 
-app.get('/balance', (req, res) => {
-    const balance = req.query.address;
-    console.log("Balance: ", balance);
-    try {
-        fs.appendFileSync('./result.txt', balance + "\n");
-    } catch (err) {
-        console.error('File Write Error: ', err);
-    }
-
-    return res.status(200).send("OK");
-})
-
 app.get('/getLPInfo', (req, res) => {
     console.log('getLPInfo');
 
@@ -63,6 +51,6 @@ app.listen(port, () => {
     console.log(`Server is running in PORT ${port}`);
 })
 
-main();
+// main();
 
 module.exports = app;
